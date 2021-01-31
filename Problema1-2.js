a274fc2250594cf1084f6e157a67eaf3ed11bbf4
leidy <leidyluchis@gmail.com>
2021-01-30 19:41:00 -0500

    Problema 1.2

---
+++ Problema1-2.js
@@ -0,0 +1,8 @@
+// n es el número de personas. 
+// a es el número de apretones de mano.
+
+let n = 27;
+let a = n*(n-1)/2;
+
+console.log("Se encuentran " + n + " personas.");
+console.log("Ocurren " + a + " apretones de mano");
